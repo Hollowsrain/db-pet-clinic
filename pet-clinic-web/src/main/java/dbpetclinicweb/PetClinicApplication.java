e945d29fb6c0f1126304c627beeab246a3d4e433
Donatas_Barkauskas <barkauskas.donatas@gmail.com>
2022-09-22 23:35:02 +0300

    implement config spring services. Closes #22.

--- pet-clinic-web/src/main/java/dbpetclinicweb/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/dbpetclinicweb/PetClinicApplication.java
@@ -3,7 +3,7 @@ package dbpetclinicweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"dbpetclinicdata", "dbpetclinicweb"})
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
