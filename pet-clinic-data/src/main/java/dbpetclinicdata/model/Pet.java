52482776f31a366f37def88712e713947fa4f6e4
Donatas_Barkauskas <barkauskas.donatas@gmail.com>
2022-09-22 10:24:34 +0300

    create and implement BaseEntity. Closes #10

--- pet-clinic-data/src/main/java/dbpetclinicdata/model/Pet.java
+++ pet-clinic-data/src/main/java/dbpetclinicdata/model/Pet.java
@@ -2,7 +2,7 @@ package dbpetclinicdata.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private LocalDate birthdate;
     private PetType petType;
     private Owner owner;
