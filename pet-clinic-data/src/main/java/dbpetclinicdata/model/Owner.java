1fba89169b7b232b211d1b24537b2cc6f47f61a5
Donatas_Barkauskas <barkauskas.donatas@gmail.com>
2022-09-29 23:29:02 +0300

    add additional properties to Owner. Closes #27

--- pet-clinic-data/src/main/java/dbpetclinicdata/model/Owner.java
+++ pet-clinic-data/src/main/java/dbpetclinicdata/model/Owner.java
@@ -2,9 +2,39 @@ package dbpetclinicdata.model;
 
 import java.util.Set;
 
-public class Owner extends Person{
+public class Owner extends Person {
 
     private Set<Pet> pets;
+
+    private String city;
+
+    private String address;
+
+    private String telephone;
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public Set<Pet> getPets() {
         return pets;
