ca2a0918d848ef7824689243a9f53fe9b0e7df1c
Donatas_Barkauskas <barkauskas.donatas@gmail.com>
2022-10-10 14:28:24 +0300

    Implement Owner JPA. Closes #34

--- pet-clinic-data/src/main/java/dbpetclinicdata/model/Pet.java
+++ pet-clinic-data/src/main/java/dbpetclinicdata/model/Pet.java
@@ -1,12 +1,24 @@
 package dbpetclinicdata.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "birth_date")
     private LocalDate birthdate;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public String getName() {
