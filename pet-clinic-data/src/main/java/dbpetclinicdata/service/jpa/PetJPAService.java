bdf40dac97a09ec2e0aca2bd5c084e05793b2497
Donatas_Barkauskas <barkauskas.donatas@gmail.com>
2022-10-10 15:40:04 +0300

    Add Pets jpa service. Closes #40

---
+++ pet-clinic-data/src/main/java/dbpetclinicdata/service/jpa/PetJPAService.java
@@ -0,0 +1,48 @@
+package dbpetclinicdata.service.jpa;
+
+import dbpetclinicdata.model.Pet;
+import dbpetclinicdata.repositories.PetRepository;
+import dbpetclinicdata.service.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("JPA")
+public class PetJPAService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElseThrow(() -> new RuntimeException("No pet found by Id: " + aLong));
+    }
+
+    @Override
+    public Pet save(Pet entity) {
+        return petRepository.save(entity);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
